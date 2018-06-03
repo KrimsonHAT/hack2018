984a1f182ae455c1e7f03d33f6a650ea7c200310
Juan Carlos Quirino <juanquirinoc@gmail.com>
2018-06-02 23:07:55 -0500

    Added create project form

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,6 +2,18 @@ function loadModal() {
   $('#myInput').trigger('focus')
 }
 
+$( "#send" ).click(function( event ) {
+
+  if ($("#project-name").val().length === 0 ||  ($("#project-name").val().length > 32)) {
+    event.preventDefault();
+    console.log("nope")
+  } else {
+    console.log($("#project-name"))
+  }
+  
+
+});
+
 var app5 = new Vue({
   el: '#exampleModalCenterTitle',
   data: {
