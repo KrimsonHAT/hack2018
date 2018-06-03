7a010f74be700c82573948787e37223eaed1cee8
Juan Carlos Quirino <juanquirinoc@gmail.com>
2018-06-03 08:58:35 -0500

    Added tasks list

--- public/javascripts/features.js
+++ public/javascripts/features.js
@@ -2,7 +2,6 @@
 
 $.getJSON('api/gFeatures', function (data) {
     //userNameInDB = data;
-    console.log(data);
     data.forEach(function (item) {
         app_features.add(item);
     });
@@ -12,17 +11,26 @@ $("#send").click(function (event) {
 
     if ($("#feature-name").val().trim().length === 0 || ($("#project-name").val().trim().length > 32)) {
         event.preventDefault();
-        console.log("nope")
     } else {
-        console.log($("#feature-name"))
     }
 });
+function add_event() {
+    $("#task_send").click(function (event) {
+        if ($("#task-description").val().trim().length === 0 || ($("#task-description").val().trim().length > 32)) {
+            event.preventDefault();
+        } else {
+        }
+    });
+}
+
 
 $.getJSON('api/gOtherUsers', function (data) {
     data.forEach(function (item) {
         $('#chooseUsers').append('<option>' + item.username + '</option>');
     });
 });
+
+
 $.getJSON('api/gProyectUsers', function (data) {
     data.forEach(function (item) {
         $("#getGroupUsers").append('<option value=' + item.username + '>' + item.username + '</option>');
@@ -65,7 +73,31 @@ var app_features = new Vue({
     methods: {
         add: function (feature) {
             this.features.push(feature);
-        }/*,
+        },
+        something: function (name, leader, id) {
+            $("#exampleModalCenterTitle").html(name);
+            $("#administrator").html(leader);
+            $("#feature_id").val(id);
+            if ($('#header').text() !== $('#administrator').text()) {
+                $('#admin-button').hide();
+            } else {
+                $('#admin-button').show();
+            }
+            add_event();
+            var myString = "";
+            $.getJSON('api/gTasks?feature=' + id, function (data) {
+                myString = ""
+                data.forEach(function (item) {
+                    //Append data
+                    myString += item.description + "\n";
+                });
+                $("#todoList").html(myString);
+                console.log($("#todoList"));
+            });
+
+        }
+
+        /*,
       enterProj: function(feature) {
         location.href="/project?project="+feature;
       }
